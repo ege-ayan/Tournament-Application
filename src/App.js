d844bc59375b3cd8d1c7fed388d8fb6160f19287
Ege Ayan <139765763+ege-ayan@users.noreply.github.com>
2023-10-19 02:12:39 +0300

    Title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import IndexPage from './components/IndexPage';
 import Matchmaker from './components/MatchMakerInput';
 import AutomaticTournament from './components/AutomaticTournament';
